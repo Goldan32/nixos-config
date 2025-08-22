05d3d808f15668235fc14a49db1aa8b6496bb59f
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-08-22 11:11:33 +0200

    First try with full rewrite

--- modules/common.nix
+++ modules/common.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  time.timeZone = "UTC";
+  time.timeZone = "Europe/Budapest";
   i18n.defaultLocale = "en_US.UTF-8";
   services.openssh.enable = true;
 
@@ -8,4 +8,24 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "dialout" "docker" ];
   };
+
+  programs.zsh.enable = true;
+  programs.neovim.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    curl
+    git
+    zip
+    xz
+    unzip
+    file
+    which
+    gnused
+    gnutar
+    gawk
+    gnupg
+    bash
+  ];
 }
