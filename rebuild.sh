53c73fb8c3856f6b529efc7683a2e6ead5c0e01c
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-10-23 10:32:19 +0200

    oops

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+if [ "$1" == "" ]; then
+    echo "rebuild.sh <target>"
+    exit 1
+fi
+
 PROOT="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 HOME_CONFIG="$PROOT/home"
 DOTFILES="$PROOT/home/dotfiles"
+TARGET="$1"
 
-sudo nixos-rebuild switch --flake "$PROOT"#server --override-input home-config path:"$HOME_CONFIG" --override-input dotfiles "$DOTFILES"
+sudo nixos-rebuild switch --flake "$PROOT"#"$TARGET" --override-input home-config path:"$HOME_CONFIG" --override-input dotfiles "$DOTFILES"
